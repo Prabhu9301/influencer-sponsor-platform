e72e6415fb2bb04d2bdce1efb73115b19921fc59
Prabhu <21f2001015@ds.study.iitm.ac.in>
2024-10-12 11:29:58 +0530

    Start v2.0 dev

--- application/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-from os import getenv
-
-load_dotenv()
-
-class Config:    
-    SECRET_KEY = getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.sqlite3'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
